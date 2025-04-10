524ae20155d090b4649be18afcc3ac1d86296e41
pm162004 <pm121614@gmail.com>
2025-04-10 12:40:56 +0530

    initial commit

---
+++ WebElementsCommands/ClickAndHold.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+import time
+driver = webdriver.Chrome()
+driver.get("https://www.geeksforgeeks.org/")
+driver.maximize_window()
+time.sleep(2)
+
+actions = ActionChains(driver) # driver pass as a parameter
+
+link = driver.find_element(By.PARTIAL_LINK_TEXT, "DSA to Development")
+
+actions.click_and_hold(link).perform()
+time.sleep(2)
+actions.release()
