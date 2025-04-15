86700f3a27f9f93374139614acbf36f7c9aac650
Priya Modi <modip5214@gmail.com>
2025-04-16 00:06:34 +0530

    initial commit

---
+++ pyTest/ASSERTIONPyTest.py
@@ -0,0 +1,5 @@
+def test_assert():
+    a=9
+    b=0
+    assert a==b,"Failed"
+    print("Passed")
---
+++ pyTest/test_sample.py
@@ -0,0 +1,8 @@
+def test_one():
+    print("Inside test_one")
+
+def test_two():
+    print("Inside test_two")
+
+def test_three():
+    print("Inside test_three")
