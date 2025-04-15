8d89d0940a2b682a356879da3c7a7fffd89aff5e
pm162004 <pm121614@gmail.com>
2025-04-15 10:25:25 +0530

    initial commit

---
+++ Training/Assertions.py
@@ -0,0 +1,19 @@
+# from selenium import webdriver
+# import time
+# driver =webdriver.Chrome()
+# driver.get()
+# driver.maximize_window()
+# time.sleep()
+# driver.find_element()
+#
+# driver.close()
+assert "Selenium" in "Selenium is for web automation","VALIDATION_ERR"
+print("Validation passed")
+
+str1 = "priya"
+str2 = "priya"
+assert str1==str2 , "Strings Matching Failed"
+print("Validation 2 Passed")
+
+assert "Selenium" in ["Selenium","Python","Java"],"VALIDATION_ERR"
+print("Validation 3 passed")
