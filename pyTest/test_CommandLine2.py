37751feb065e814b59fa518ad0d8cbd0590c0565
Priya Modi <modip5214@gmail.com>
2025-04-20 21:21:05 +0530

    initial commit

--- pyTest/test_CommandLine2.py
+++ pyTest/test_CommandLine2.py
@@ -17,12 +17,13 @@ class TestCLI:
 
     def test_Login(self,setup):
         self.driver=setup
-        self.driver.get("https://opensource-demo.orangehrmlive.com/")
-        self.driver.find_element(By.NAME, "txtUsername").send_keys("Admin")
-        self.driver.find_element(By.ID, "txtPassword").send_keys("admin123")
-        self.driver.find_element(By.ID, "btnLogin").click()
+        self.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        self.driver.implicitly_wait(30)
+        self.driver.find_element(By.NAME, "username").send_keys("Admin")
+        self.driver.find_element(By.NAME, "password").send_keys("admin123")
+        self.driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
         try:
-            self.status = self.driver.find_element(By.XPATH, "//h1[normalize-space()='Dashboard']").is_displayed()
+            self.status = self.driver.find_element(By.XPATH, "//h6[normalize-space()='Dashboard']").is_displayed()
             self.driver.close()
             assert self.status == True
         except:
