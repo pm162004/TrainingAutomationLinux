04cdd82b438cc34b75fbf76d0da3c9d2ba0d7e7c
pm162004 <pm121614@gmail.com>
2025-04-21 17:52:35 +0530

    initial commit

--- Evalution/Task7.py
+++ Evalution/Task7.py
@@ -4,20 +4,18 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import re
 
 
 driver = webdriver.Chrome()
-driver.implicitly_wait(20)
+
 
 
 driver.get("https://dev.webelight.co.in/contact-us")
 driver.maximize_window()
 
+wait = WebDriverWait(driver, 20)
 
-time.sleep(2)
-
-# driver.execute_script('window.scrollBy(0,500)',"")
+driver.execute_script('window.scrollBy(0,500)',"")
 
 
 btn = driver.find_element(By.XPATH, "//img[@alt='logo']")
@@ -25,6 +23,7 @@ btn = driver.find_element(By.XPATH, "//img[@alt='logo']")
 
 driver.execute_script("window.scrollBy(0, 2300);",btn)
 time.sleep(2)
-btn1 = driver.find_element(By.XPATH,"//a[contains(text(),'Blog')]")
-driver.execute_script("arguments[0].click;",btn1)
+checkbox = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[text()='Blog']")))
+driver.execute_script("arguments[0].click();", checkbox)
+
 time.sleep(2)
