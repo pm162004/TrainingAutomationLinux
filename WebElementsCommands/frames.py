5afe8b0fb6040e993c6380a00879c46512057c15
pm162004 <pm121614@gmail.com>
2025-05-14 17:27:56 +0530

    Merge remote-tracking branch 'origin/master'

--- WebElementsCommands/frames.py
+++ WebElementsCommands/frames.py
@@ -1,15 +1,26 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-
+from selenium.common.exceptions import TimeoutException
+# Code not working
 driver = webdriver.Chrome()
-import time
 driver.maximize_window()
 driver.get("https://yopmail.com/en/wm")
-time.sleep(2)
-driver.switch_to.frame("ifmail")
-driver.find_element(By.LINK_TEXT,'View My Booking').click()
-driver.switch_to.default_content()
 
+try:
+    # Wait up to 10 seconds for the frame to be available
+    wait = WebDriverWait(driver, 10)
+    frame = wait.until(EC.frame_to_be_available_and_switch_to_it("ifmail"))
+    
+    # Wait for the link to be clickable
+    booking_link = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'View My Booking')))
+    booking_link.click()
+    
+    # Switch back to default content
+    driver.switch_to.default_content()
 
+except TimeoutException:
+    print("Frame or element not found within the specified time")
+finally:
+    driver.quit()
